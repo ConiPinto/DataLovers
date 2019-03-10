8c014ef5b680e64519ddc76cfa75fede3774ff93
ConiPinto <c.pinjazz@gmail.com>
2019-03-09 23:51:03 -0300

    Se añadió filter en data.js corregido

--- src/data.js
+++ src/data.js
@@ -1,8 +1,22 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
+/*const example = () => {
   return 'example';
 };
 
-window.example = example;
+window.example = example;*/
+
+
+//filter
+
+const filterAuthor = (showAll, condition) => {
+  
+  const filteredAuthor= showAll.filter(element => {
+      return element.author === condition
+  })
+  return filteredAuthor;
+ }
+ window.filterAuthor = filterAuthor;
+
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 const rootContainer = document.getElementById('root');
 const showAll = (STEAM.appnews.newsitems);
-	
-
-	
+const selectByTitle = document.getElementById("btnfilter");
+const orderBy = document.getElementById("btnorder")
 	
 const showData = (showAll) => { 
 let result = '';
@@ -14,11 +13,9 @@ let result = '';
 	    <div class='box'>
 	    <h4>${element.title}</h4>
 	    </div>
-			
 			<h6><span>Autor: Without author</span></h6>
 			<h6>Feed Label: ${element.feedlabel}</h6>
 			<h6>${element.appid}</h6>
-			
 	    </div>
 	    </div>
 	    </div>`
@@ -29,11 +26,9 @@ let result = '';
 			<div class='box'>
 			<h4>${element.title}</h4>
 	    </div>
-			
 			<h6><span>${element.author}</span></h6>
 			<h6>Feed Label: ${element.feedlabel}</h6>
 			<h6>${element.appid}</h6>
-			
 	    </div>
 	    </div>
 	    </div>` 
@@ -41,5 +36,7 @@ let result = '';
 	  });
 return result;
 }
-
 window.onload = showData(showAll);
+
+
+
