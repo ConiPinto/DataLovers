ebd6bb35421490f5ccaa6c0aceddce1e4061f174
ConiPinto <c.pinjazz@gmail.com>
2019-03-10 00:15:57 -0300

    Order en data.js listo, con correciones y funcionando

--- src/data.js
+++ src/data.js
@@ -19,4 +19,37 @@ const filterAuthor = (showAll, condition) => {
  }
  window.filterAuthor = filterAuthor;
 
+//order
+
+const selectOrder = (showAll, condition) => {
+  //console.log(condition);
+  /* const sorted = showAll.sort((a, b) => (a[condition] > b[condition]) ? 1 : -1);
+  return sorted; */
+
+  
+  if (condition === 'title1'){
+    showAll.sort(function (a,b){
+      if(a.title > b.title){
+        return 1;
+      }
+      if (a.title < b.title){
+        return -1;
+      }
+      return 0;
+    });
+  }else{
+    showAll.sort(function (a,b){
+      if(a.title < b.title){
+        return 1;
+      }
+      if (a.title > b.title){
+        return -1;
+      }
+      return 0;
+    });
+  }
+return showAll;
+
+ }
+ window.selectOrder = selectOrder;
 
