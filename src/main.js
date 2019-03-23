3af18e2a0fae4d1448fbde61c8eb15994159b3d9
ConiPinto <c.pinjazz@gmail.com>
2019-03-23 18:10:18 -0300

    agregando manejo del DOM para calculo

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ const rootContainer = document.getElementById('root');
 const showAll = (STEAM.appnews.newsitems);
 const selectByTitle = document.getElementById("btnfilter");
 const orderBy = document.getElementById("btnorder")
+const calculateResult = document.getElementById("calculate");
+
 	
 const showData = (showAll) => { 
 let result = '';
@@ -19,24 +21,29 @@ let result = '';
 	    </div>
 	    </div>
 	    </div>`
-	  } else {
-	    result = rootContainer.innerHTML += `
-	    <div>
-	    <div class='card'>
-			<div class='box'>
-			<h4>${element.title}</h4>
-	    </div>
-			<h6><span>${element.author}</span></h6>
-			<h6>Feed Label: ${element.feedlabel}</h6>
-			<h6>${element.appid}</h6>
-	    </div>
-	    </div>
-	    </div>` 
-	  }
+	  } 
 	  });
 return result;
 }
 window.onload = showData(showAll);
+
+
+const resultCalculate = document.getElementById("btnfilter");
+resultCalculate.addEventListener("change", ()=>{
+
+let condition = resultCalculate.value;
+let percentage = window.computerStart(showAll,condition);
+
+calculateResult.innerHTML = "";
+
+calculateResult.innerHTML+= `
+<div>
+<h1>La cantidad de tarjetas mostradas es de un ${percentage}</h1>
+</div>`
+
+})
+
+
 
 
 
