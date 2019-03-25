b6ac5788c095a26c12b6edba01759856a84516cc
ConiPinto <c.pinjazz@gmail.com>
2019-03-24 21:12:40 -0300

    Commit para actualizar todo

--- src/main.js
+++ src/main.js
@@ -10,17 +10,17 @@ let result = '';
  showAll.forEach(element => {
 if (element.author === '' && element.feedlabel !== '') {
 result = rootContainer.innerHTML += `
-	    <div>
-	    <div class='card'>
-	    <div class='box'>
-	    <h4>${element.title}</h4>
-	    </div>
-			<h6><span>Autor: Without author</span></h6>
-			<h6>Feed Label: ${element.feedlabel}</h6>
-			<h6>${element.appid}</h6>
-	    </div>
-	    </div>
-	    </div>`
+<div>
+<div class='card'>
+<div class='box'>
+<h4>${element.title}</h4>
+</div>
+<h6><span>Autor: Without author</span></h6>
+<h6>Feed Label: ${element.feedlabel}</h6>
+<h6>${element.appid}</h6>
+</div>
+</div>
+</div>`
 } 
 });
 return result;
